a8a80adcc677d87533681b52cfbcf4bb42261bda
zhenyiplusone <68820910+zhenyiplusone@users.noreply.github.com>
2023-04-27 13:27:24 -0400

    redux

---
+++ src/components/controls.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import { useDispatch } from 'react-redux';
+
+import { increment, decrement } from '../actions';
+
+function Controls(props) {
+  const dispatch = useDispatch();
+  return (
+    <div>
+      <button type="button" onClick={() => dispatch(increment())}>+</button>
+      <button type="button" onClick={() => dispatch(decrement())}>-</button>
+    </div>
+  );
+}
+
+export default Controls;
---
+++ src/components/counter.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import { useSelector } from 'react-redux';
+
+function Counter(props) {
+  const count = useSelector((store) => store.count);
+  return (
+    <div>
+      Current Count: {count}
+    </div>
+  );
+}
+
+export default Counter;
