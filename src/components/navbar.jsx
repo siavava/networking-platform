d0d9441b8edf6b060dacac6342090c307af7c5d4
Bansharee Ireen <bansharee.ireen.24@dartmouth.edu>
2023-05-20 16:40:50 -0400

    navbar now changes appropriately

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,18 +1,37 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 
 export default function Nav() {
+  const location = useLocation();
+  const path = location.pathname;
+  if (path === '/' || path === '/login' || path === '/signup') {
+    return (
+      <nav>
+        <ul className="navbar">
+          <li><NavLink to="/">Goloco</NavLink></li>
+
+          <div className="inner-navbar">
+            <li><NavLink to="/signup">Sign Up</NavLink></li>
+            <li><NavLink to="/login">Log In</NavLink></li>
+          </div>
+          {/* <li><NavLink to="/test/id1">test id1</NavLink></li>
+          <li><NavLink to="/test/id2">test id2</NavLink></li> */}
+        </ul>
+      </nav>
+    );
+  }
   return (
     <nav>
       <ul className="navbar">
         <li><NavLink to="/">Goloco</NavLink></li>
 
         <div className="inner-navbar">
-          <li><NavLink to="/signup">Sign Up</NavLink></li>
-          <li><NavLink to="/login">Log In</NavLink></li>
+          <li><NavLink to="/companies">Companies</NavLink></li>
+          <li><NavLink to="/people">People</NavLink></li>
+          <li><NavLink to="/profile">Profile Photo</NavLink></li>
         </div>
         {/* <li><NavLink to="/test/id1">test id1</NavLink></li>
-        <li><NavLink to="/test/id2">test id2</NavLink></li> */}
+          <li><NavLink to="/test/id2">test id2</NavLink></li> */}
       </ul>
     </nav>
   );
