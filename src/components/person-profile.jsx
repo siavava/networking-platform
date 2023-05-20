768f768da02cc0f456de1d76d11a15718e17a06a
Bansharee Ireen <bansharee.ireen.24@dartmouth.edu>
2023-05-20 17:36:08 -0400

    added markdown display for basic information and structured expected data for person profile page

--- src/components/person-profile.jsx
+++ src/components/person-profile.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
+import ReactMarkdown from 'react-markdown';
 
 export default function PersonProfile() {
   const user = {
     fname: 'Chad',
     lname: 'III',
-    photo: 'url',
+    photo: 'https://source.unsplash.com/random/200x200/?img=1',
+    company: 'Google',
     email: 'giga.chad@dartmouth.edu',
     connectionType: 'homie',
-    notes: '# Notes About Chad III: \n - chad works at meta \n - loves beer \n - enjoys a solid workout',
+    // "# THIS IS A GRIM BABY\n\n* very soft\n* very cute\n* i want himb"
+    notes: '# Notes About Chad III:\n- chad works at meta\n- loves beer\n- enjoys a solid workout',
   };
 
   const tasks = [
@@ -25,7 +28,12 @@ export default function PersonProfile() {
     <div className="person-profile">
       <div className="first-row">
         <div className="basic-info">
+          <img src={user.photo} alt="profile" />
           <h1>{`${user.fname} ${user.lname}`}</h1>
+          <p>{user.company}</p>
+          <p>{user.email}</p>
+          <p>{`Connection Type: ${user.connectionType}`}</p>
+          <ReactMarkdown>{user.notes}</ReactMarkdown>
         </div>
         <div className="todos">
           <h1>Tasks/To Dos</h1>
