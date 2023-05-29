57bf45eeac216c393dac20e82eab860bfedf2c12
zhenyiplusone <yizhen.zhen.24@dartmouth.edu>
2023-05-28 22:38:51 -0400

    landing page

--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import '../landing-page.style.scss';
 
 export default function Landing() {
   return (
-    <div className="landing-page" />
+    <div className="landing-page">
+      <img src="src\img\logo_inverted.png" alt="logo" />
+      <p>A Modern Solution For Networking</p>
+      <button type="button"><NavLink to="/signup">Join Now!</NavLink></button>
+    </div>
   );
 }
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -11,8 +11,7 @@ export default function Nav() {
           <li><NavLink to="/">Goloco</NavLink></li>
 
           <div className="inner-navbar">
-            <li><NavLink to="/signup">Sign Up</NavLink></li>
-            <li><NavLink to="/login">Log In</NavLink></li>
+            <li className="authbtns"><NavLink to="/login">Log In</NavLink></li>
           </div>
         </ul>
       </nav>
@@ -28,7 +27,7 @@ export default function Nav() {
         <div className="inner-navbar">
           <li><NavLink to="/companies">COMPANIES</NavLink></li>
           <li><NavLink to="/people">PEOPLE</NavLink></li>
-          <li id="signoutbtn"><NavLink to="/signout">SIGNOUT</NavLink></li>
+          <li className="authbtns"><NavLink to="/signout">SIGNOUT</NavLink></li>
         </div>
       </ul>
     </nav>
