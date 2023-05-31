b7251984f76d675306ec799b3afba2012bb58860
Bansharee Ireen <bansharee.ireen.24@dartmouth.edu>
2023-05-31 14:39:41 -0400

    finished styling

--- src/components/delete-modal.jsx
+++ src/components/delete-modal.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { deleteCompany } from '../store/actions';
+import '../delete.style.scss';
 
 export default function DeleteCompanyModal(props) {
   const dispatch = useDispatch();
@@ -21,9 +22,9 @@ export default function DeleteCompanyModal(props) {
       <div className="task-modal">
         <div className="task-modal-content">
           Are you sure you want to delete?
-          <div className="submit-btn">
-            <button type="button" onClick={handleDeleteCompany}>Yes</button>
-            <button type="button" onClick={() => closeModal()}>No</button>
+          <div className="delete-options">
+            <button type="button" onClick={handleDeleteCompany} className="delete-options-buttons">Yes</button>
+            <button type="button" onClick={() => closeModal()} className="delete-options-buttons">No</button>
           </div>
         </div>
       </div>
