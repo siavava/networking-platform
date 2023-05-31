b569f22dc526b2ef8885fdc4396ac6f0f42012ba
Bansharee Ireen <bansharee.ireen.24@dartmouth.edu>
2023-05-30 23:10:35 -0400

    adding delete modal and moving delete functionality to modal page

--- src/components/old.jsx
+++ src/components/old.jsx
@@ -18,6 +18,7 @@ export default function CompanyProfile() {
   const [isDeleteCompanyModalOpen, setIsDeleteCompanyModalOpen] = useState(false);
   const [isCreatePersonModalOpen, setIsCreatePersonModalOpen] = useState(false);
   const openModal = (type) => {
+    console.log(type);
     if (type === 'company') setIsCreateCompanyModalOpen(true);
     else if (type === 'person') setIsCreatePersonModalOpen(true);
     else if (type === 'delete-company') setIsDeleteCompanyModalOpen(true);
