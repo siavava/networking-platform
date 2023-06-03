27c40c69c64bf8f54c4447d542226631d706d2f2
siavava <amittaijoel@outlook.com>
2023-06-03 18:38:46 -0400

    people grid/list view component update

--- src/components/people.jsx
+++ src/components/people.jsx
@@ -13,6 +13,7 @@ export default function People() {
   const dispatch = useDispatch();
   const [searchTerm, setSearchTerm] = useState('');
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isGridView, setIsGridView] = useState(true);
 
   const search = (term) => {
     if (term) {
@@ -64,35 +65,60 @@ export default function People() {
         {/* list vs. grid view toggle */}
         <div className="people-list-grid-toggle">
           <button type="button" onClick={openModal}>Create</button>
-          <button type="button"> List </button>
-          <button type="button"> Grid </button>
+          <button type="button" onClick={() => setIsGridView(false)}> List </button>
+          <button type="button" onClick={() => setIsGridView(true)}> Grid </button>
         </div>
       </div>
 
       {/* main panel -- contains list of people */}
       <div className="people-main-panel">
-        <ul className="people-list">
-          { people.map((person) => (
-            <li key={person.id} className="people-list-item">
-              <div className="flip-card">
-                <div className="flip-card-inner">
-                  <div className="flip-card-front">
-                    <img src={person.imageUrl} alt="profile" />
+        { isGridView
+          ? (
+            <ul className="people-grid-view">
+              { people.map((person) => (
+                <li key={person.id} className="people-list-item">
+                  <div className="flip-card">
+                    <div className="flip-card-inner">
+                      <div className="flip-card-front">
+                        <img src={person.imageUrl} alt="profile" />
+                      </div>
+                      <div className="flip-card-back">
+                        <h1 className="person-list-item-name">{person.name}</h1>
+                        <p className="person-list-item-title">{person.title}</p>
+                        <p className="person-list-item-email">{person.email}</p>
+                        <p className="person-list-item-description">{person.description}</p>
+                      </div>
+                    </div>
                   </div>
-                  <div className="flip-card-back">
-                    <h1 className="person-list-item-name">{person.name}</h1>
-                    <p className="person-list-item-title">{person.title}</p>
-                    <p className="person-list-item-email">{person.email}</p>
-                    <p className="person-list-item-description">{person.description}</p>
+                  <button className="person-list-item-button" type="button" onClick={() => handleShowPerson(person.id)}>
+                    to see full page
+                  </button>
+                </li>
+              ))}
+            </ul>
+          )
+          : (
+            <ul className="people-list-view">
+              { people.map((person) => (
+                <li key={person.id} className="people-list-item">
+                  <div className="people-list-card">
+                    <div className="person-image">
+                      <img src={person.imageUrl} alt="profile" />
+                    </div>
+                    <div className="person-info">
+                      <h1 className="person-name">{person.name}</h1>
+                      <p className="person-list-item-title">{person.title}</p>
+                      <a className="person-email" href={`mailto:${person.email}`}>{person.email}</a>
+                      <p className="person-description">{person.description}</p>
+                    </div>
                   </div>
-                </div>
-              </div>
-              <button className="person-list-item-button" type="button" onClick={() => handleShowPerson(person.id)}>
-                to see full page
-              </button>
-            </li>
-          ))}
-        </ul>
+                  <button className="person-more-info-button" type="button" onClick={() => handleShowPerson(person.id)}>
+                    to see full page
+                  </button>
+                </li>
+              ))}
+            </ul>
+          ) }
       </div>
 
       {isModalOpen && (
